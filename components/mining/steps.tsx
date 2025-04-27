b9c2cfb34857a1588e64bf873ccc67b2f952941d
Meyse <maxtheyse@pm.me>
2025-04-27 16:32:31 +0100

    fix link

--- components/mining/steps.tsx
+++ components/mining/steps.tsx
@@ -150,7 +150,7 @@ export function MiningSteps() {
             </div>
 
             <a 
-              href="https://docs.verus.io/mining/mining-software.html" 
+              href="https://docs.verus.io/economy/start-mining.html#mining-software)" 
               target="_blank"
               rel="noopener noreferrer"
               className="group h-[40px] md:h-[50px] px-8 bg-white/80 dark:bg-blue-950/80 backdrop-blur-sm border border-blue-200 dark:border-blue-800/60 rounded-lg text-[14px] md:text-[16px] font-medium text-verus-blue dark:text-blue-300 flex items-center justify-center hover:border-blue-300 dark:hover:border-blue-700 transition-all duration-300 hover:shadow-lg dark:hover:shadow-blue-950/50 hover:text-blue-600 dark:hover:text-blue-200 hover:-translate-y-[1px] w-fit mt-8 gap-2"
