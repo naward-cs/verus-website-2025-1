6ea5ef59ea7c98e83790b84af84bd9153221bc64
Meyse <maxtheyse@pm.me>
2025-04-27 18:05:41 +0100

    update for verus.io OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,11 @@ import type { Metadata } from 'next'
 import { Header } from '@/components/header'
 import { geomanist } from './fonts'
 import { ThemeProvider } from '@/context/ThemeContext'
-import { getSiteUrl } from '@/lib/utils'
 
 export const metadata: Metadata = {
   title: 'The Internet of Value | Verus',
   description: 'A fundamentally different and better protocol to build Web3, and to empower individuals & communities.',
-  metadataBase: new URL('https://nextdev.verus.io'),
+  metadataBase: new URL('https://verus.io'),
 }
 
 export default function RootLayout({
