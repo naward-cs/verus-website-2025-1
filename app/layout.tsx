974dd2fdfe3acaf9ae5c6e2b9faf9122f18d514c
Meyse <maxtheyse@pm.me>
2025-04-27 13:25:49 +0100

    opengraph fix layout.tsx staging site

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { getSiteUrl } from '@/lib/utils'
 export const metadata: Metadata = {
   title: 'The Internet of Value | Verus',
   description: 'A fundamentally different and better protocol to build Web3, and to empower individuals & communities.',
-  metadataBase: new URL(getSiteUrl()),
+  metadataBase: new URL('https://nextdev.verus.io'),
 }
 
 export default function RootLayout({
