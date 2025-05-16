4dccbb490bce0dea493a0f6057ce5a844489297a
naward-cs <nicholas.ward.cyber@gmail.com>
2025-05-16 10:30:59 -0500

    configure eslint to function correctly, update gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,25 @@
 //@ts-check
-import path, {dirname} from 'node:path'
-import {fileURLToPath} from 'node:url'
+import path from 'node:path'
 
-import {includeIgnoreFile} from '@eslint/compat'
-import {FlatCompat} from '@eslint/eslintrc'
+import { includeIgnoreFile } from '@eslint/compat'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
-  // baseDirectory: import.meta.dirname,
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
+
 })
 
 /**
  * All packages that leverage t3-env should use this rule
  */
 const restrictEnvAccess = tseslint.config(
-  {ignores: ['**/env.ts']},
+  { ignores: [ '**/env.ts' ] },
   {
-    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    files: [ '**/*.js', '**/*.ts', '**/*.tsx' ],
 
     rules: {
       'no-restricted-properties': [
@@ -37,7 +35,7 @@ const restrictEnvAccess = tseslint.config(
         'error',
         {
           name: 'process',
-          importNames: ['env'],
+          importNames: [ 'env' ],
           message:
             "Use `import { env } from '@/configs/env'` instead to ensure validated types.",
         },
@@ -46,37 +44,46 @@ const restrictEnvAccess = tseslint.config(
   }
 )
 
+
+
+
 const eslintConfig = [
   includeIgnoreFile(path.join(import.meta.dirname, '.gitignore')),
-  {
-    files: ['src/**/*', '**/*.js', '**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+  { ignores: [ '**/*.config.*', '.next/**' ] },
+  { files: [ '**/*.{js,mjs,cjs,ts,jsx,tsx}' ] },
+  ...tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    {
+      files: [ 'src/**/*', '**/*.js', '**/*.ts', '**/*.tsx' ],
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
     },
-  },
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  ),
 
+  ...compat.config({
+    extends: [ 'next/core-web-vitals', 'next/typescript', 'prettier' ],
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-html-link-for-pages': 'error',
-      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+      'import/consistent-type-specifier-style': [ 'error', 'prefer-top-level' ],
       /** typescript rules */
       '@typescript-eslint/no-unused-vars': [
         'error',
-        {argsIgnorePattern: '^_', varsIgnorePattern: '^_'},
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
       '@typescript-eslint/consistent-type-definitions': 'off',
       '@typescript-eslint/consistent-type-imports': [
         'warn',
-        {prefer: 'type-imports', fixStyle: 'separate-type-imports'},
+        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
       ],
       '@typescript-eslint/no-misused-promises': [
         2,
-        {checksVoidReturn: {attributes: false}},
+        { checksVoidReturn: { attributes: false } },
       ],
       '@typescript-eslint/no-unnecessary-condition': [
         'error',
@@ -85,7 +92,7 @@ const eslintConfig = [
         },
       ],
       '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-elstin/no-require-imports': 'warn',
+      '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
