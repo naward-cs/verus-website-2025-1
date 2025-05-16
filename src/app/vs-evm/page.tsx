f13c48fd580d43016215a1fa2c69bebb30dbc25c
naward-cs <nicholas.ward.cyber@gmail.com>
2025-05-16 11:37:09 -0500

    vs-evm route refectored

--- src/app/vs-evm/page.tsx
+++ src/app/vs-evm/page.tsx
@@ -1,7 +1,9 @@
-import {Metadata} from 'next'
+import type {Metadata} from 'next'
 
+import {ComparisonTable} from '@/features/vs-evm/comparison-table'
+
+import {BodyBgSmallImages} from '@/components/background-images'
 import {Footer} from '@/components/footer'
-import {ComparisonTable} from '@/components/vs-evm/comparison-table'
 
 export const metadata: Metadata = {
   title: 'VM-based Blockchains vs Verus | Verus',
@@ -32,17 +34,7 @@ export const metadata: Metadata = {
 export default function VsEvmPage() {
   return (
     <main className="relative mt-[50px] h-[calc(100vh-50px)] w-screen md:mt-[70px] md:h-[calc(100vh-70px)]">
-      {/* Background images - separate for light and dark mode */}
-      <img
-        src="/img/bg-small.webp"
-        alt="Background - light"
-        className="fixed absolute h-full w-full -translate-y-[300px] object-cover dark:hidden md:-translate-y-[50px]"
-      />
-      <img
-        src="/img/bg-small-dark.webp"
-        alt="Background - dark"
-        className="fixed absolute hidden h-full w-full -translate-y-[300px] object-cover dark:block md:-translate-y-[50px]"
-      />
+      <BodyBgSmallImages />
 
       <div className="relative z-10 flex min-h-[calc(100vh-50px)] flex-col md:min-h-[calc(100vh-70px)]">
         <div className="flex-grow py-8 md:py-16">
